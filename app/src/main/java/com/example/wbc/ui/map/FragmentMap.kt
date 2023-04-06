bcc14cf167ffa4d8d8eeb82e18beb6fa9a534480
spicypunch <miniguni66@naver.com>
2023-04-06 23:21:31 +0900

    tablayout, viewpager2 적용

--- app/src/main/java/com/example/wbc/ui/map/FragmentMap.kt
+++ app/src/main/java/com/example/wbc/ui/map/FragmentMap.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.wbc.databinding.FragmentMapBinding
+import net.daum.mf.map.api.MapView
 
 class FragmentMap() : Fragment() {
 
@@ -18,7 +20,16 @@ class FragmentMap() : Fragment() {
     ): View? {
         binding = FragmentMapBinding.inflate(inflater, container, false)
 
-        binding.sectionLabel.text = "지도지도지도지도지도지도지도지도"
         return binding.root
+    }
+
+    override fun onResume() {
+
+        val mapView = MapView(activity)
+
+        val mapViewContainer = binding.mapView as ViewGroup
+
+        mapViewContainer.addView(mapView)
+        super.onResume()
     }
 }
---
+++ app/src/main/java/com/example/wbc/ui/search/FragmentSearch.kt
@@ -0,0 +1,4 @@
+package com.example.wbc.ui.search
+
+class FragmentSearch {
+}
