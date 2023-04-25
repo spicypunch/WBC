bf66d578c674884058427d7212309a1ddb4d0e15
minig <miniguni66@naver.com>
2023-04-25 17:25:12 +0900

    마커 이미지 및 기능 추가

--- app/src/main/java/com/example/wbc/ui/map/MapActivity.kt
+++ app/src/main/java/com/example/wbc/ui/map/MapActivity.kt
@@ -7,19 +7,17 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import com.example.wbc.R
 import com.example.wbc.databinding.ActivityMapBinding
 import com.example.wbc.di.coroutine.IoDispatcher
 import com.example.wbc.ui.search.SearchViewModel
-import com.google.gson.Gson
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.*
 import net.daum.mf.map.api.MapPOIItem
 import net.daum.mf.map.api.MapPoint
 import net.daum.mf.map.api.MapView
-import java.io.BufferedReader
-import java.io.InputStreamReader
-import java.util.*
 import javax.inject.Inject
 
 @AndroidEntryPoint
@@ -27,29 +25,24 @@ class MapActivity () : AppCompatActivity() {
     @Inject
     @IoDispatcher
     lateinit var ioDispatcher: CoroutineDispatcher
-
     private lateinit var binding: ActivityMapBinding
-
     private val mapViewModel: MapViewModel by viewModels()
-
     private val searchViewModel: SearchViewModel by viewModels()
-
     // 위도
     private var latitude: Double = 0.0
     // 경도
     private var longitude: Double = 0.0
-
     private val mapView by lazy {
         MapView(this)
     }
-
+    private val eventListener by lazy {
+        MarkerEventListener(this)
+    }
     private val marker = MapPOIItem()
-
     private val permissionList = arrayOf(
         android.Manifest.permission.ACCESS_COARSE_LOCATION,
         android.Manifest.permission.ACCESS_FINE_LOCATION,
     )
-
     private val requestMultiplePermission =
         registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { result ->
             result.forEach {
@@ -58,79 +51,97 @@ class MapActivity () : AppCompatActivity() {
                 }
             }
         }
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMapBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
         // 권한 확인
         requestMultiplePermission.launch(permissionList)
-
+        // 카카오맵 띄우기
+        val mapViewContainer = binding.mapView as ViewGroup
+        mapView.setPOIItemEventListener(eventListener)
+        mapViewContainer.addView(mapView)
         // 현재 위치
         getMyLocation()
-
         // 현재 위치 근방의 버스 정류소 출력
         mapViewModel.getBusStationInfo(latitude, longitude)
-
         // Search Fragment에서 넘어온 값 처리
         if (intent.hasExtra("address")) {
             val address = intent.getStringExtra("address")
             if (address != null) {
                 mapViewModel.searchLocation(address, latitude, longitude)
-                setMarker(latitude, longitude, address)
+                setMarker(latitude, longitude, address, false)
             }
-
         }
-
-        // 카카오맵 띄우기
-        val mapViewContainer = binding.mapView as ViewGroup
-        mapViewContainer.addView(mapView)
-
         // 현재 위치로 이동 버튼
         binding.fabMap.setOnClickListener {
             getMyLocation()
         }
-
         // 검색 버튼
         binding.btnSearch.setOnClickListener {
             if (binding.editSearch.text.toString().isBlank()) {
                 Toast.makeText(this, "주소를 입력해주세요", Toast.LENGTH_SHORT).show()
-                mapViewModel.getBusArrivalTime("GGB206000108")
             } else {
                 mapViewModel.searchLocation(binding.editSearch.text.toString(), latitude, longitude)
                 // 검색 기록 추가
                 searchViewModel.insertHistory(binding.editSearch.text.toString())
             }
         }
-
-
-
         // 검색 결과
         mapViewModel.searchResult.observe(this, androidx.lifecycle.Observer {
             if (it.documents.isEmpty()) {
                 Toast.makeText(this, "2km 근방에 찾으신 시설이 없습니다.", Toast.LENGTH_SHORT).show()
             } else {
-                setMarker(it.documents[0].y.toDouble(), it.documents[0].x.toDouble(), it.documents[0].place_name)
+                setMarker(it.documents[0].y.toDouble(), it.documents[0].x.toDouble(), it.documents[0].place_name, false)
                 mapViewModel.getBusStationInfo(it.documents[0].y.toDouble(), it.documents[0].x.toDouble())
             }
         })
-
         // 버스 정류장 출력
         mapViewModel.busStationResult.observe(this, androidx.lifecycle.Observer {
             if (it.body?.items?.item?.isEmpty() == true) {
                 Toast.makeText(this, "근처에 버스 정류장이 없습니다.", Toast.LENGTH_SHORT).show()
             } else {
                 for (i in it.body?.items?.item!!) {
-                    setMarker(i.gpsLati!!, i.gpsLong!!, i.nodeNm!!)
+                    setMarker(i.gpsLati!!, i.gpsLong!!, i.nodeNm!!, true)
                 }
             }
         })
-
         mapViewModel.busArrivalTimeResult.observe(this, androidx.lifecycle.Observer {
             Log.e("result", it.toString())
         })
+    }
+
+    class MarkerEventListener(val context: Context): MapView.POIItemEventListener {
+        override fun onPOIItemSelected(p0: MapView?, p1: MapPOIItem?) {
+//            Toast.makeText(context, "onPOIItemSelected", Toast.LENGTH_SHORT).show()
+        }
+
+        override fun onCalloutBalloonOfPOIItemTouched(p0: MapView?, p1: MapPOIItem?) {
+//            Toast.makeText(context, "onCalloutBalloonOfPOIItemTouched", Toast.LENGTH_SHORT).show()
+        }
+
+        override fun onCalloutBalloonOfPOIItemTouched(
+            p0: MapView?,
+            p1: MapPOIItem?,
+            p2: MapPOIItem.CalloutBalloonButtonType?
+        ) {
+            val builder = AlertDialog.Builder(context)
+            val itemList = arrayOf("토스트", "마커 삭제", "취소")
+            builder.setTitle("${p1?.itemName}")
+            builder.setItems(itemList) { dialog, which ->
+                when(which) {
+                    0 -> Toast.makeText(context, "토스트", Toast.LENGTH_SHORT).show()  // 토스트
+                    1 -> p0?.removePOIItem(p1)    // 마커 삭제
+                    2 -> dialog.dismiss()   // 대화상자 닫기
+                }
+            }
+            builder.show()
+        }
+
+        override fun onDraggablePOIItemMoved(p0: MapView?, p1: MapPOIItem?, p2: MapPoint?) {
+//            Toast.makeText(context, "onDraggablePOIItemMoved", Toast.LENGTH_SHORT).show()
+        }
+
     }
 
     private fun getMyLocation() {
@@ -138,22 +149,27 @@ class MapActivity () : AppCompatActivity() {
         latitude = locationProvider.getLocationLatitude()
         longitude = locationProvider.getLocationLongitude()
 
-        setMarker(latitude, longitude, "현재 위치")
+        setMarker(latitude, longitude, "현재 위치", false)
     }
 
     // 지도에 마커 생성
-    private fun setMarker(latitude: Double, longitude: Double, title: String) {
+    private fun setMarker(latitude: Double, longitude: Double, title: String, busMarker: Boolean) {
         mapView.setMapCenterPoint(MapPoint.mapPointWithGeoCoord(latitude, longitude), true);
-        marker.itemName = title
-        marker.mapPoint = MapPoint.mapPointWithGeoCoord(latitude, longitude)
-
-        if (title != "현재 위치") {
-            marker.markerType = MapPOIItem.MarkerType.BluePin
-            marker.selectedMarkerType = MapPOIItem.MarkerType.RedPin
-        } else {
-            marker.markerType = MapPOIItem.MarkerType.RedPin
+        marker.apply {
+            itemName = title
+            mapPoint = MapPoint.mapPointWithGeoCoord(latitude, longitude)
+            if (busMarker) {
+                markerType = MapPOIItem.MarkerType.CustomImage
+                selectedMarkerType = MapPOIItem.MarkerType.CustomImage
+                customImageResourceId = R.drawable.icon_bus
+                customSelectedImageResourceId = R.drawable.icon_bus
+                isCustomImageAutoscale = false
+                setCustomImageAnchor(0.5f, 1.0f)
+            } else {
+                markerType = MapPOIItem.MarkerType.BluePin
+                selectedMarkerType = MapPOIItem.MarkerType.RedPin
+            }
         }
-
         mapView.addPOIItem(marker)
     }
 }
